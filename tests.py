4d25e47bd22f1f34d90bd515414e3c9db7b7379f
David Durost <david.durost@gmail.com>
2025-11-19 19:44:15 -0500

    Add landing-page smoke test for anonymous users

--- tests.py
+++ tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class PagesViewTests(TestCase):
+    def test_index_renders_for_anonymous_users(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Campfire Connections")
